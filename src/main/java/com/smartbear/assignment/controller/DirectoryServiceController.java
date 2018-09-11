4821968438369ae84d98c024de59a539e7571fd0
tonmoyroy <tonmoy.iitg@gmail.com>
2018-09-11 04:01:32 +0200

    Read and Delete API Added

--- src/main/java/com/smartbear/assignment/controller/DirectoryServiceController.java
+++ src/main/java/com/smartbear/assignment/controller/DirectoryServiceController.java
@@ -11,6 +11,7 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -19,7 +20,7 @@ public class DirectoryServiceController {
     @Autowired
     private DirectoryServiceRepository directoryServiceRepository;
 
-    @GetMapping("/ping")
+    @GetMapping("ping")
     public String ping() throws Exception{
 
         return "Application is working!";
@@ -33,6 +34,13 @@ public class DirectoryServiceController {
         return new ResponseEntity<Entry>(newentry, HttpStatus.OK);
     }
 
+
+    @GetMapping("read")
+    public List<Entry> readEntry() throws Exception{
+        return directoryServiceRepository.findAll();
+    }
+
+
     @PutMapping("update/{id}")
     public Entry updateEntry(@PathVariable Long id, @Valid @RequestBody Entry entry) throws Exception{
 
@@ -42,5 +50,15 @@ public class DirectoryServiceController {
             var.setEmail(entry.getEmail());
             return directoryServiceRepository.save(var);
         }).orElseThrow(() -> new ResourceNotFoundException("No Such Entry Exists with id " + id));
+    }
+
+    @DeleteMapping("delete/{email}")
+    public ResponseEntity<?> deleteEntry(@PathVariable String email) throws Exception{
+        Entry item = directoryServiceRepository.findByEmail(email);
+        if(!item.equals(null)){
+            directoryServiceRepository.delete(item);
+            return ResponseEntity.status(HttpStatus.OK).body(email+" Deleted Successfully!");
+        }else
+            throw new RuntimeException("No Such Entry Exists with id " + email);
     }
 }
--- src/main/java/com/smartbear/assignment/repository/DirectoryServiceRepository.java
+++ src/main/java/com/smartbear/assignment/repository/DirectoryServiceRepository.java
@@ -4,4 +4,5 @@ import com.smartbear.assignment.model.Entry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DirectoryServiceRepository extends JpaRepository<Entry, Long> {
+    Entry findByEmail(String email);
 }
